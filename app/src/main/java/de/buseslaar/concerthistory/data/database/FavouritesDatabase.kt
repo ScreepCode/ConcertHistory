b0fe8cd1964c5df42b9121387f74d6c66b8f198a
Jozys <joshua@slaar.de>
2024-11-20 16:27:18 +0100

    ♻️ Add DAOs to FavouritesDatabase
    - Add ArtistDao to FavouritesDatabase
    - Add SetlistDao to FavouritesDatabase

--- app/src/main/java/de/buseslaar/concerthistory/data/database/FavouritesDatabase.kt
+++ app/src/main/java/de/buseslaar/concerthistory/data/database/FavouritesDatabase.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import de.buseslaar.concerthistory.data.database.dao.ArtistDao
+import de.buseslaar.concerthistory.data.database.dao.SetlistDao
 import de.buseslaar.concerthistory.data.database.entity.Artist
 import de.buseslaar.concerthistory.data.database.entity.Setlist
 
 @Database(entities = [Artist::class, Setlist::class], version = 1, exportSchema = false)
 abstract class FavouritesDatabase : RoomDatabase() {
+    abstract val artistDao: ArtistDao
+    abstract val setlistDao: SetlistDao
 
     companion object {
         @Volatile
