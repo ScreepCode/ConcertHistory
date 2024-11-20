7530a0445799971314d8403b2681e6f5e7ee2f84
Niklas Buse <nicki_bu@gmx.de>
2024-11-20 14:41:34 +0100

    ✨Add Last Concerts to Dashboard View

--- app/src/main/java/de/buseslaar/concerthistory/views/dashboard/DashboardViewModel.kt
+++ app/src/main/java/de/buseslaar/concerthistory/views/dashboard/DashboardViewModel.kt
@@ -1,15 +1,45 @@
 package de.buseslaar.concerthistory.views.dashboard
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import de.buseslaar.concerthistory.data.datastore.DataStoreServiceProvider
+import de.buseslaar.concerthistory.data.remote.dto.SetListDto
+import de.buseslaar.concerthistory.data.remote.service.UserService
 import de.buseslaar.concerthistory.utils.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 
 class DashboardViewModel : BaseViewModel() {
+    private val userService = UserService()
+    private val dataStore = DataStoreServiceProvider.getInstance()
+
+    private var userName: String? = null
+
     private val _menuExpanded = MutableStateFlow(false)
     val menuExpanded: StateFlow<Boolean> = _menuExpanded.asStateFlow()
 
+    var lastAttendedConcerts: MutableList<SetListDto> by mutableStateOf(mutableListOf())
+
     fun onMenuExpandedChange(expanded: Boolean) {
         _menuExpanded.value = expanded
+    }
+
+    fun initialize() {
+        asyncRequest {
+            userName = dataStore.setlistUsername.first()
+            if (!isUserNameProvided()) {
+                throw Exception("Username is missing")
+            }
+            lastAttendedConcerts =
+                (userName?.let { userService.getUserAttended(it).setlist }
+                    ?: emptyList()).toMutableList()
+        }
+    }
+
+    fun isUserNameProvided(): Boolean {
+        return userName != null && userName!!.isNotBlank()
     }
 }
