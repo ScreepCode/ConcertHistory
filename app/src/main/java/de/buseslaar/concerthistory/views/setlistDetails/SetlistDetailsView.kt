198924e42e9461829cd11aaabd63b771462fa445
Niklas Buse <nicki_bu@gmx.de>
2024-11-28 18:50:44 +0100

    ✨ Added favorites to concert details

--- app/src/main/java/de/buseslaar/concerthistory/views/setlistDetails/SetlistDetailsView.kt
+++ app/src/main/java/de/buseslaar/concerthistory/views/setlistDetails/SetlistDetailsView.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
@@ -37,6 +39,7 @@ import de.buseslaar.concerthistory.data.remote.dto.SetListDto
 import de.buseslaar.concerthistory.data.remote.dto.SetsDto
 import de.buseslaar.concerthistory.data.remote.dto.SongDto
 import de.buseslaar.concerthistory.ui.parts.LoadingIndicator
+import kotlin.reflect.KFunction0
 
 @Composable
 fun SetlistDetailsView(
@@ -59,7 +62,9 @@ fun SetlistDetailsView(
             SetlistDetailsAppBar(
                 menuExpanded = menuExpanded,
                 onMenuExpandedChange = viewModel::onMenuExpandedChange,
-                setListFmUrl = selectedSetlist?.url
+                setListFmUrl = selectedSetlist?.url,
+                isSetlistLiked = viewModel.isLiked,
+                onLikeClick = viewModel::onLikeToggle,
             )
         }
     ) { innerPadding ->
@@ -177,11 +182,21 @@ private fun SongEntry(
 private fun SetlistDetailsAppBar(
     menuExpanded: Boolean,
     onMenuExpandedChange: (Boolean) -> Unit,
-    setListFmUrl: String? = null
+    setListFmUrl: String? = null,
+    isSetlistLiked: Boolean,
+    onLikeClick: KFunction0<Unit>
 ) {
     TopAppBar(title = {
         Text(stringResource(R.string.setlist_details))
     }, actions = {
+        IconButton(onClick = { onLikeClick() }) {
+            Icon(
+                imageVector = if (isSetlistLiked) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                contentDescription = if (isSetlistLiked) stringResource(R.string.isLiked) else stringResource(
+                    R.string.isNotLiked
+                ),
+            )
+        }
         MoreMenu(
             menuExpanded = menuExpanded,
             onMenuExpandedChange = { onMenuExpandedChange(it) },
--- app/src/main/java/de/buseslaar/concerthistory/views/setlistDetails/SetlistDetailsViewModel.kt
+++ app/src/main/java/de/buseslaar/concerthistory/views/setlistDetails/SetlistDetailsViewModel.kt
@@ -1,6 +1,11 @@
 package de.buseslaar.concerthistory.views.setlistDetails
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import de.buseslaar.concerthistory.data.database.repository.SetlistRepository
 import de.buseslaar.concerthistory.data.datastore.DataStoreServiceProvider
+import de.buseslaar.concerthistory.data.mapper.reduceToEntity
 import de.buseslaar.concerthistory.data.remote.dto.SetListDto
 import de.buseslaar.concerthistory.data.remote.service.UserService
 import de.buseslaar.concerthistory.utils.BaseViewModel
@@ -12,8 +17,11 @@ import kotlinx.coroutines.flow.first
 class SetlistDetailsViewModel : BaseViewModel() {
     private val userService = UserService()
     private val dataStore = DataStoreServiceProvider.getInstance()
+    private val favoritesRepository = SetlistRepository()
+
     private var userName: String? = null
     var selectedSetlist: SetListDto? = null
+    var isLiked by mutableStateOf(false)
 
     private val _menuExpanded = MutableStateFlow(false)
     val menuExpanded: StateFlow<Boolean> = _menuExpanded.asStateFlow()
@@ -25,10 +33,35 @@ class SetlistDetailsViewModel : BaseViewModel() {
             selectedSetlist = (userName?.let { userService.getUserAttended(it).setlists }
                 ?: emptyList()).find { it.id == selectedSetlistId }
                 ?: throw Exception("Setlist not found")
+
+            isLiked = favoritesRepository.getSetlistById(selectedSetlistId) != null
         }
     }
 
     fun onMenuExpandedChange(expanded: Boolean) {
         _menuExpanded.value = expanded
+    }
+
+    fun onLikeToggle() {
+        if (isLiked) {
+            removeConcertFromFavorites()
+        } else {
+            addConcertToFavorites()
+        }
+        isLiked = !isLiked
+    }
+
+    fun addConcertToFavorites() {
+        asyncRequest {
+            favoritesRepository.insert(selectedSetlist!!.reduceToEntity())
+        }
+    }
+
+    fun removeConcertFromFavorites() {
+        asyncRequest {
+            favoritesRepository.getSetlistById(selectedSetlist!!.id)?.let {
+                favoritesRepository.delete(it)
+            }
+        }
     }
 }
