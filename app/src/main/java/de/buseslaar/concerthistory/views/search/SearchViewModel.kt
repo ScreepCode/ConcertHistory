26b356943c39b93dd01bf0f90d773a22ec3ff00a
Jozys <joshua@slaar.de>
2024-12-05 12:48:09 +0100

    ♻️ Implement error handling for search and dashboard
    Implements error handling for search and dashboard views, providing specific error messages based on error codes. Handles network connectivity issues and provides appropriate feedback to the user.

--- app/src/main/java/de/buseslaar/concerthistory/views/search/SearchViewModel.kt
+++ app/src/main/java/de/buseslaar/concerthistory/views/search/SearchViewModel.kt
@@ -1,8 +1,12 @@
 package de.buseslaar.concerthistory.views.search
 
+import AppContextHolder
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import de.buseslaar.concerthistory.R
 import de.buseslaar.concerthistory.data.database.entity.Artist
 import de.buseslaar.concerthistory.data.database.entity.Setlist
 import de.buseslaar.concerthistory.data.database.repository.ArtistRepository
@@ -14,6 +18,11 @@ import de.buseslaar.concerthistory.data.remote.service.ArtistService
 import de.buseslaar.concerthistory.data.remote.service.SetlistService
 import de.buseslaar.concerthistory.utils.BaseViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 class SearchViewModel : BaseViewModel() {
     var tabIndex: SearchTab by mutableStateOf(SearchTab.SETLISTS)
@@ -29,40 +38,55 @@ class SearchViewModel : BaseViewModel() {
     // Artist search
     var artists by mutableStateOf(emptyList<ArtistDto>())
     var artistSearchText by mutableStateOf("")
-    var artistErrorMessage: String by mutableStateOf("")
-
+    var artistErrorMessage: Int? by mutableStateOf(null)
     var artistTextFieldFocused by mutableStateOf(false)
 
     // Concert search
     var concerts by mutableStateOf(emptyList<SetListDto>())
     var concertSearchText by mutableStateOf("")
-    var concertErrorMessage by mutableStateOf("")
-
+    var concertErrorMessage by mutableStateOf<Int?>(null)
+    private var concertErrorCode: Int? by mutableStateOf<Int?>(null)
     var concertTextFieldFocused by mutableStateOf(false)
 
     fun initialize(initialTab: SearchTab) {
         tabIndex = initialTab
+
+        if (!isConnected()) {
+            concertErrorMessage = R.string.search_connection_timeout
+            artistErrorMessage = R.string.search_connection_timeout
+        }
     }
 
     fun searchArtist() {
         asyncRequest(onError = { exception ->
-            artistErrorMessage = exception.message ?: "Unknown error"
+            artistErrorMessage = parseErrorCode(getErrorCode(exception.message ?: ""))
         }) {
             artists = emptyList()
-            artistErrorMessage = ""
-            artists = artistService.searchArtist(artistSearchText).artists
+            artistErrorMessage = null
+            if (isConnected()) {
+                artists = artistService.searchArtist(artistSearchText).artists
+            } else {
+                artistErrorMessage = parseErrorCode(408)
+            }
         }
     }
 
     fun searchConcerts() {
         asyncRequest(
             onError = { exception ->
-                concertErrorMessage = exception.message ?: "Unknown error"
+                concertErrorMessage = parseErrorCode(getErrorCode(exception.message ?: ""))
             }
         ) {
             concerts = emptyList()
-            concertErrorMessage = ""
-            concerts = setlistService.searchSetlist(concertSearchText).setlists
+            concertErrorMessage = null
+            concertErrorCode = null
+            if (isConnected()) {
+                concerts = setlistService.searchSetlist(concertSearchText).setlists
+            } else {
+                concertErrorMessage = parseErrorCode(408)
+            }
+
+
         }
     }
 
@@ -91,6 +115,29 @@ class SearchViewModel : BaseViewModel() {
             artistFavoritesRepository.getArtistByMbid(artist.mbid)?.let {
                 artistFavoritesRepository.delete(it)
             }
+        }
+    }
+
+    private fun getErrorCode(json: String): Int {
+        try {
+            return Json.decodeFromString<JsonObject>(json).jsonObject["code"]?.jsonPrimitive?.int
+                ?: 0
+        } catch (_: Exception) {
+            return 0
+        }
+    }
+
+    private fun isConnected(): Boolean {
+        return (AppContextHolder.getInstance().getApplicationContext()
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo != null
+    }
+
+    private fun parseErrorCode(code: Int): Int {
+        return when (code) {
+            404 -> R.string.search_no_results_found
+            408 -> R.string.no_internet_title
+            500 -> R.string.search_internal_server_error
+            else -> R.string.search_unknown_error
         }
     }
 }
